7ce65c50b05025a465dab7502e9515c0a1c54bcd
Daniel Rios <danielrios.oficial@gmail.com>
2016-12-20 12:26:15 -0200

    flexbox responsive align fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+//Gulp tasks for sass
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('watch');
 
+//Compile only style file with the same name, the imports will be added on file compilled
 //Sass outputs: expanded, compact, compressed
 
 gulp.task('sass', function() {
@@ -10,9 +13,12 @@ gulp.task('sass', function() {
     .pipe(gulp.dest(''));
 });
 
+//Watch all files including all that are imported, than complile using 'sass' task
+
 gulp.task('watch', function() {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
+//execute the task inside the array only with 'gulp' command
 
 gulp.task('default', ['watch']);
