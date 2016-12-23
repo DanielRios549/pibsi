625fc90caa42f0573f88b012fbf566694b79f77e
Daniel Rios <danielrios.oficial@gmail.com>
2016-12-23 06:42:31 -0200

    Bower added

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php get_header();?>
 	<main id="principalSection">
 		<section id="containerLeft">
-			<?php if(have_posts()) { while(have_posts()) { the_post();?>
+			<?php if(have_posts()) { if(is_home() && !is_front_page()) {?>
+				<header class="postTitle">
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php } while(have_posts()) { the_post(); get_template_part('template-parts/content', get_post_format());?>
 			<article class="contentDiv">
 				<a class="thumbnail" href="<?php echo the_permalink();?>"><?php the_post_thumbnail('thumbnail');?></a>
 				<h1 class="title">
